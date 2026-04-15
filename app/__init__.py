061f8431949157d6f82af853a6ae67cf912633d2
IsaacGonzalez19 <121037773@upq.edu.mx>
2026-04-15 00:35:56 -0600

    Se agrega el archivo __init__.py para que el archivo run.py apunte a ese archivo

---
+++ app/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from app.extensions.db import db
+from app.extensions.migrate import migrate
+
+def create_app():
+    app = Flask(__name__)
+
+    from app.config.config import Config
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    return app
