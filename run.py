4370aabf364d5eb18d64d2b4a98bc482355c98fb
IsaacGonzalez19 <121037773@upq.edu.mx>
2026-04-15 14:41:01 -0600

    Se elimino solo un elemento para verificar si la conexión de la base de datos era exitosa

--- run.py
+++ run.py
@@ -1,16 +1,8 @@
 from app import create_app
-from app.extensions.db import db
-from sqlalchemy import text
+
 
 app = create_app()
 
-@app.route("/")
-def test_db():
-    try:
-        result = db.session.execute(text("SELECT 1"))
-        return f"Conexión exitosa: {list(result)}"
-    except Exception as e:
-        return f"Error de conexión: {str(e)}"
 
 
 if __name__ == "__main__":
