30898d0ea8812007379b6b536a4943bb5f622918
IsaacGonzalez19 <121037773@upq.edu.mx>
2026-04-15 13:21:57 -0600

    Solamente información personal del usuario.

---
+++ app/models/persona.py
@@ -0,0 +1,13 @@
+from app.extensions.db import db
+from .base import BaseModel
+
+class Persona(BaseModel):
+    __tablename__ = "personas"
+
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100), nullable=False)
+    apellido_paterno = db.Column(db.String(100), nullable=False)
+    apellido_materno = db.Column(db.String(100), nullable=False)
+
+    usuario = db.relationship("Usuario", back_populates="persona", uselist=False)
+
