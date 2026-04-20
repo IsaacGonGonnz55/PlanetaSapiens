37fb65bb3d8203138b945fad8506b9eb35aebc86
IsaacGonGonnz55 <ig1264609@gmail.com>
2026-04-20 15:10:36 -0600

    Se implementó el gestor de inicio de sesión con carga de usuario y mensajes de advertencia.

--- app/extensions/login_manager.py
+++ app/extensions/login_manager.py
@@ -3,3 +3,9 @@ from flask_login import LoginManager
 login_manager = LoginManager()
 login_manager.login_view = "auth.login" 
 login_manager.login_message = "Debes iniciar sesión primero"
+login_manager.login_message_category = "warning"
+
+@login_manager.user_loader
+def load_user(user_id):
+    from app.models.usuario import Usuario  # import interno (evita ciclos)
+    return Usuario.query.get(int(user_id))
