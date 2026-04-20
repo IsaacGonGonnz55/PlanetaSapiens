1570484e930f542ba72ad346dab5512082e6f7f6
IsaacGonGonnz55 <ig1264609@gmail.com>
2026-04-20 15:11:04 -0600

    Se eliminó la redirección a dashboard en el inicio de sesión y se corrigió la redirección a la página principal tras el inicio exitoso.

--- app/modules/auth/routes.py
+++ app/modules/auth/routes.py
@@ -2,14 +2,12 @@ from flask import render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, login_required, current_user
 from . import auth_bp
 from app.modules.auth.services.auth_service import autenticar_usuario
+from app.modules.auth.services.session_service import autenticar_sesion
 
 
 
 @auth_bp.route("/login", methods=["GET", "POST"])
 def login():
-    if current_user.is_authenticated:
-        return redirect(url_for("main.dashboard"))
-
     if request.method == "POST":
         identifier = request.form.get("identifier")
         password = request.form.get("password")
@@ -18,20 +16,21 @@ def login():
 
         if user:
             login_user(user)
-            return redirect(url_for("main.dashboard"))
+            autenticar_sesion(user)
+            return redirect(url_for("main.home"))
 
         flash("Credenciales inválidas", "danger")
 
     return render_template("public/auth/login.html")
 
 
-# 📝 REGISTER (vista simple por ahora)
+# REGISTER (vista simple por ahora)
 @auth_bp.route("/register")
 def register():
     return render_template("public/auth/registro.html")
 
 
-# 🚪 LOGOUT
+# LOGOUT
 @auth_bp.route("/logout")
 @login_required
 def logout():
