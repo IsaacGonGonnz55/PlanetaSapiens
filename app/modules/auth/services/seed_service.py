d6766133c58cce8c83d56628964170d35378b3fe
IsaacGonGonnz55 <ig1264609@gmail.com>
2026-04-21 00:22:28 -0600

    Se movió la función crear_super_admin al módulo seed_service y se eliminó su llamada en seed_all para mejorar la estructura del código.

--- app/modules/auth/services/seed_service.py
+++ app/modules/auth/services/seed_service.py
@@ -2,14 +2,46 @@
 
 from .role_service import crear_tipos_usuario
 from .permission_service import crear_permisos, asignar_permisos
-from .user_service import crear_super_admin 
+from app.extensions.db import db
+from app.models.usuario import Usuario
+from app.models.tipo_usuario import TipoUsuario
+from app.models.persona import Persona
 
 
 def seed_all():
     crear_tipos_usuario()
     crear_permisos()
     asignar_permisos()
-    crear_super_admin()
 
     print("Sistema inicializado correctamente") 
-    
+
+
+def crear_super_admin():
+    if Usuario.query.filter_by(es_superadmin=True).first():
+        print("⚠️ Ya existe un super admin")
+        return
+
+    rol = TipoUsuario.query.filter_by(nombre="SUPER_ADMIN").first()
+
+    persona = Persona(
+        nombre="Super",
+        apellido_paterno="Admin",
+        apellido_materno="Sistema"
+    )
+    db.session.add(persona)
+    db.session.flush()
+
+    user = Usuario(
+        persona_id=persona.id,
+        username="superadmin",
+        email="admin@planetasapiens.com",
+        es_superadmin=True,
+        tipo_usuario=rol
+    )
+
+    user.set_password("93Wag826-#)c")
+
+    db.session.add(user)
+    db.session.commit()
+
+    print("Super admin creado")    
